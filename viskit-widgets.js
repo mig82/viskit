c1e8cee4a34223b4da06248edeee756fbb04f0d9
Miguelangel Fernandez Mendoza <miguelangelxfm@gmail.com>
2018-11-16 15:17:33 +0100

    Implement the widget count subcommand

--- viskit-widgets.js
+++ viskit-widgets.js
@@ -1,20 +1,91 @@
 #!/usr/bin/env node
 
+//TODO: Count heavy widgets per form -e.g. segments, browsers, maps.
+//TODO: Detect non-page-level heavy widgets.
+//TODO: Find deprecated widgets: com.kony.gen.viz.model.container.KVizHBox/KVizVBox
+
 const program = require('commander');
+const find = require('find');
+const fs = require('fs-extra');
 const colors = require('colors');
-colors.setTheme({
-	prompt: 'grey',
-	info: 'cyan',
-	warn: 'yellow',
-	error: 'red'
-});
+const theme = require('./viskit').theme;
+colors.setTheme(theme);
+
+//var uiTypes = ['forms', 'templates', 'userwidgets'];
+var uiTypes = [
+	'forms',
+	'popups',
+	'templates',
+	'userwidgets'
+];
+
+var result = {
+	'form-count': 0,
+};
 
 program
-	.command('count [channel]')
-	.description('Produce a count of widgets per form for a specific channel (mobile|tablet|web|wearables)')
-	.action(channel => {
-		console.log('Counting widgets for channel %s'.info, channel);
+	.command('count <project>')
+	.description('Produce a count of widgets per form')
+	.action(countProjectWidgets);
+
+function countProjectWidgets(project){
+
+	if(project.substr(-1) !== '/'){
+		project += '/';
+	}
+	console.log('Counting widgets for project %s'.info, project);
+
+	//Find any form, template or component UI -i.e. Any directory with the .sm extension.
+	//If the project structure changes in the future the quick fix is to look in the entire project.
+	//find.dir(/\.sm$/, project, exploreForms);
+
+	uiTypes.forEach(uiType => {
+		//Search in path/to/project/forms, path/to/project/templates, etc.
+		var uiTypePath = project + uiType;
+
+		fs.pathExists(uiTypePath)
+		.then(exists => {
+			if(exists){
+				find.dir(/\.sm$/, uiTypePath, (forms) => {
+					exploreForms(uiType, uiTypePath, forms);
+				});
+			}
+			else{
+				console.log("Path %s does NOT exist".warn, uiTypePath);
+			}
+		});
 	});
+}
+
+function exploreForms(uiType, uiTypePath, forms){
+	console.log('%s: %d'.info, uiType, forms.length);
+	forms.forEach((formPath) => {
+		countFormWidgets(uiTypePath, formPath);
+	});
+}
+
+function countFormWidgets(uiTypePath, formPath){
+
+	//var formSplit = formPath.split('/');
+	//var formDisplayName = formSplit[formSplit.length - 1];
+
+	var uiTypePathRegex = new RegExp("^" + uiTypePath);
+	var formDisplayName = formPath.replace(uiTypePathRegex, '');
+	//console.log('\t\t%s'.debug, formDisplayName);
+
+	find.file(formPath, (widgets) => {
+		var widgetCount = widgets.length;
+		if(widgetCount <= 200){
+			console.log('\t%s: %d'.green, formDisplayName, widgetCount);
+		}
+		else if(widgetCount > 400){
+			console.log('\t%s: %d'.red, formDisplayName, widgetCount);
+		}
+		else{
+			console.log('\t%s: %d'.yellow, formDisplayName, widgetCount);
+		}
+	})
+}
 
 if (!process.argv.slice(2).length) {
 	program.help(helpText => {
@@ -23,7 +94,3 @@ if (!process.argv.slice(2).length) {
 }
 
 program.parse(process.argv);
-
-//Count widgets per form.
-//Count heavy widgets per form -e.g. segments, browsers, maps.
-//Detect non-page-level heavy widgets.
--- viskit.js
+++ viskit.js
@@ -2,18 +2,41 @@
 
 const program = require('commander');
 const colors = require('colors');
-colors.setTheme({
+
+//To be exported so that the theme can be defined once.
+const theme = {
 	prompt: 'grey',
+	debug: 'magenta',
 	info: 'cyan',
 	warn: 'yellow',
 	error: 'red'
-});
+};
+colors.setTheme(theme);
 
 program
 	.version('1.0.0', '-v, --version')
 	.option('-n, --noisy', 'Output everything');
 
 program
+	//.option('-p, --project', 'Path to the Visualizer project')
+	/*.command(
+		'actions',
+		'Audit and help with actions for a specific channel')
+	.command(
+		'fonts',
+		'Audit and help with fonts for a specific channel')
+	.command(
+		'forms',
+		'Audit and help with forms for a specific channel')
+	.command(
+		'i18ns',
+		'Audit and help with i18n\'s')
+	.command(
+		'images',
+		'Audit and help with images')
+	.command(
+		'skins',
+		'Audit and help with skins for a specific theme')*/
 	.command(
 		'widgets',
 		'Audit and help with widgets for a specific channel');
@@ -31,5 +54,9 @@ if (!process.argv.slice(2).length) {
 	console.error('Invalid command: %s\nSee --help for a list of available commands.', program.args.join(' '));
 	process.exit(1);
 });*/
+
+module.exports = {
+	theme: theme
+};
 
 program.parse(process.argv);
