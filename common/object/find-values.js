794491d6e64ee62f672e25b2238e09a6f06cef37
mig82 <miguelangelxfm@gmail.com>
2019-01-22 13:53:39 +0100

    Def method to find key,value pairs in an object

--- common/object/find-values.js
+++ common/object/find-values.js
@@ -1,10 +1,6 @@
 const forOwn = require("lodash.forown");
 const indexOfStringIgnoreCase = require("../array/index-of-string-ignore-case");
-
-function NotAnObjectError(message){
-	this.name = "NotAnObjectError";
-	this.message = message;
-}
+const NotAnObjectError = require("./errors/not-an-object");
 
 function findValuesForKeys(json, searchKeys){
 
@@ -25,10 +21,10 @@ function findValuesForKeys(json, searchKeys){
 	var found = new Set();
 	var items;
 
-	if(item instanceof Array){
+	if(Array.isArray(json)){
 		items = json;
 	}
-	else /*if(item instanceof Object)*/{
+	else /*if(json instanceof Object)*/{
 		items = [json];
 	}
 
@@ -38,7 +34,7 @@ function findValuesForKeys(json, searchKeys){
 		var item = items[i];
 
 		forOwn(item, (value, key) => {
-			if(primitives.indexOf(typeof value) && indexOfStringIgnoreCase(searchKeys, key) >= 0){
+			if(primitives.indexOf(typeof value) >= 0 && indexOfStringIgnoreCase(searchKeys, key) >= 0){
 				found.add(value);
 			}
 			else if(value instanceof Array){
@@ -71,10 +67,10 @@ function findValuesMatching(json, regex){
 	var found = new Set();
 	var items;
 
-	if(item instanceof Array){
+	if(Array.isArray(json)){
 		items = json;
 	}
-	else /*if(item instanceof Object)*/{
+	else /*if(json instanceof Object)*/{
 		items = [json];
 	}
 
@@ -96,7 +92,55 @@ function findValuesMatching(json, regex){
 	return found;
 }
 
+function findEntriesMatching(json, regex){
+
+	//Only [] and {} are allowed.
+	if(typeof json !== "object"){
+		throw new NotAnObjectError("This is not an object: " + json);
+	}
+
+	//If the caller is searching for a single key, wrap it in an array.
+	if(typeof regex === "string"){
+		regex = new RegExp(regex);
+	}
+
+	if(!regex instanceof RegExp){
+		throw new Error("Cannot search for " + typeof regex);
+	}
+
+	var found = new Set();
+	var items;
+
+	if(Array.isArray(json)){
+		items = json;
+	}
+	else /*if(json instanceof Object)*/{
+		items = [json];
+	}
+
+	for (var i = 0; i < items.length; i++) {
+		var item = items[i];
+
+		forOwn(item, (value, key) => {
+			if(typeof value === "string" && regex.test(value)){
+				found.add({
+					key: key,
+					value: value
+				});
+			}
+			else if(value instanceof Array){
+				items = items.concat(value);
+			}
+			else if(value instanceof Object){
+				items.push(value);
+			}
+		});
+	}
+	return found;
+}
+
 module.exports = {
 	findValuesForKeys: findValuesForKeys,
-	findValuesMatching: findValuesMatching
+	findValuesMatching: findValuesMatching,
+	findEntriesMatching: findEntriesMatching
 };
