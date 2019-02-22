44f8778eb1176daec59fcd8b78e33d360b5862a4
mig82 <miguelangelxfm@gmail.com>
2019-02-23 00:59:00 +0100

    Put things back to original state after svv spec

--- spec/cli/viskit-set-vis-version-spec.js
+++ spec/cli/viskit-set-vis-version-spec.js
@@ -1,9 +1,12 @@
 "use strict";
 
 var cmd = "svv";
+const fs = require("fs-extra");
 const invokeCommand = require("../../src/core/helpers/invoke-command.js");
 
 describe("\tGiven a Vis 8.3 enterprise installation and a 7.3 enterprise project\n", () => {
+
+	var visEntV83Path = "./test/installations/KonyVisualizerEnterprise8.3.0";
 
 	beforeAll(()=>{
 		spyOn(console, 'log').and.callThrough();
@@ -14,12 +17,12 @@ describe("\tGiven a Vis 8.3 enterprise installation and a 7.3 enterprise project
 		//TODO: Delete test/projects/enterprise-v7.3/.viskit/ivysettings.xml
 	});
 
-	it( `\tWhen we invoke viskit ${cmd} on them\n` +
+	xit( `\tWhen we invoke viskit ${cmd} on them\n` +
 		"\tThen the 7.3 plugins are placed in the 8.3's dropins directory", async () => {
 
 			var options = [
 				cmd,
-				"./test/installations/KonyVisualizerEnterprise8.3.0",
+				visEntV83Path,
 				"./test/projects/enterprise-v7.3",
 				"-o",
 				"j",
@@ -29,5 +32,24 @@ describe("\tGiven a Vis 8.3 enterprise installation and a 7.3 enterprise project
 			var verbose = false;
 			var code = await invokeCommand("viskit", options, verbose);
 			expect(code).toBe(0);
-	}, 15000);
+			//TODO: Do some counts to verify the plugins are correctly shuffled.
+	}, 15000).pend("This test is a heavy one.");;
+
+	afterEach(async () => {
+
+		//Let's put everything back the way it was.
+		try{
+			//Remove the dropins dir.
+			await fs.remove(`${visEntV83Path}/Kony_Visualizer_Enterprise/dropins`);
+			//Remove the plugins dir.
+			await fs.remove(`${visEntV83Path}/Kony_Visualizer_Enterprise/plugins`);
+			//Rename the backup as the plugins dir.
+			await fs.move(`${visEntV83Path}/Kony_Visualizer_Enterprise/plugins_BACKUP`,
+				`${visEntV83Path}/Kony_Visualizer_Enterprise/plugins`
+			);
+		}
+		catch(e){
+			console.error(e.message);
+		}
+	});
 });
