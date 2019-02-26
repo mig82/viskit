e85595549f232ae474a1600e9d43c9e27e013dd4
mig82 <miguelangelxfm@gmail.com>
2019-02-26 11:57:31 +0100

    Add widget event action to test with

--- test/projects/StarterV8SP4/controllers/mobile/Form1Controller/Form1ControllerActions.js
+++ test/projects/StarterV8SP4/controllers/mobile/Form1Controller/Form1ControllerActions.js
@@ -1,7 +1,10 @@
-define({ 
-
- /* 
- This is an auto generated file and any modifications to it may result in corruption of the action sequence.
- */
-
+define({
+    /*
+      This is an auto generated file and any modifications to it may result in corruption of the action sequence.
+    */
+    /** onClick defined for fooButton **/
+    AS_Button_a8bfa9510246463ab518f540d0279e13: function AS_Button_a8bfa9510246463ab518f540d0279e13(eventobject) {
+        var self = this;
+        kony.print("Button clicked");
+    }
 });
