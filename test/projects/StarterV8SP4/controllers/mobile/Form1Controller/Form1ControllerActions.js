178b58a95d8133fef6b0d27f5252ea26d0c6fcad
mig82 <miguelangelxfm@gmail.com>
2019-02-27 13:37:59 +0100

    Add a reference to a broken action to test on
    Notice that while doing this Visualizer cleaned up the dead code for the
    unused action that had been left behind in the prior commit, but did not
    eliminate the JSON file for said action.

--- test/projects/StarterV8SP4/controllers/mobile/Form1Controller/Form1ControllerActions.js
+++ test/projects/StarterV8SP4/controllers/mobile/Form1Controller/Form1ControllerActions.js
@@ -12,14 +12,13 @@ define({
         var self = this;
         kony.print("Form1 postShow")
     },
-    /** onTextChange defined for fooTextField **/
-    AS_TextField_d7c5129e329e4c3b81487f7589116440: function AS_TextField_d7c5129e329e4c3b81487f7589116440(eventobject, changedtext) {
-        var self = this;
-        kony.print("fooTextField onTextChange")
-    },
     /** onDone defined for fooTextField **/
     AS_TextField_i652a4d48c7f4db9a030a23d1ba215cf: function AS_TextField_i652a4d48c7f4db9a030a23d1ba215cf(eventobject, changedtext) {
         var self = this;
         kony.print("fooTextField onDone")
+    },
+    /** onTouchEnd defined for fooButton **/
+    AS_Button_icfa4e0af4b54dafb28ac7ca3933e8a0: function AS_Button_icfa4e0af4b54dafb28ac7ca3933e8a0(eventobject, x, y) {
+        var self = this;
     }
 });
