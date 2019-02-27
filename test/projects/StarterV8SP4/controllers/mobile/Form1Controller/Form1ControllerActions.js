a974ed7b83a73095ceb99ef91e5a6c4c37cd6406
mig82 <miguelangelxfm@gmail.com>
2019-02-27 13:02:07 +0100

    Add another form event and a widget event to test on

--- test/projects/StarterV8SP4/controllers/mobile/Form1Controller/Form1ControllerActions.js
+++ test/projects/StarterV8SP4/controllers/mobile/Form1Controller/Form1ControllerActions.js
@@ -2,9 +2,19 @@ define({
     /*
       This is an auto generated file and any modifications to it may result in corruption of the action sequence.
     */
-    /** onClick defined for fooButton **/
+    /** onclick defined for fooButton **/
     AS_Button_a8bfa9510246463ab518f540d0279e13: function AS_Button_a8bfa9510246463ab518f540d0279e13(eventobject) {
         var self = this;
         kony.print("Button clicked");
+    },
+    /** postShow defined for Form1 **/
+    AS_Form_cb62e9621751426182a4b457429b9f7a: function AS_Form_cb62e9621751426182a4b457429b9f7a(eventobject) {
+        var self = this;
+        kony.print("Form1 postShow")
+    },
+    /** onDone defined for fooTextField **/
+    AS_TextField_i652a4d48c7f4db9a030a23d1ba215cf: function AS_TextField_i652a4d48c7f4db9a030a23d1ba215cf(eventobject, changedtext) {
+        var self = this;
+        kony.print("fooTextField onDone")
     }
 });
