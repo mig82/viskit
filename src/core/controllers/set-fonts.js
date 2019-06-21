274e02d5fd9b62771ad949df43a38c05cf9591d5
mig82 <miguelangelxfm@gmail.com>
2019-06-21 13:49:02 +0200

    Return results for set-fonts command

--- src/core/controllers/set-fonts.js
+++ src/core/controllers/set-fonts.js
@@ -9,6 +9,8 @@ const findSkinFiles = require("../operations/find-skin-files");
 const getChannelFromPlatform = require("../config/channels").getChannelFromPlatform;
 
 async function setFonts(font, projectPath, channel, theme, fontsWhitelist, force, verbose){
+
+	var results = [];
 	//console.log(`Channel: ${channel}`)
 	if(verbose)console.log(`Excepted fonts: ${JSON.stringify(fontsWhitelist)}`.debug);
 
@@ -21,31 +23,40 @@ async function setFonts(font, projectPath, channel, theme, fontsWhitelist, force
 	if(typeof theme === "undefined"){
 		theme = "defaultTheme";
 	}
-	console.log("channel:"+channel)
+	//console.log("channel:"+channel)
 
 	//Find all the skin files which we'll have to update.
 	var skinFiles = await findSkinFiles(projectPath, theme, verbose);
 
 	for (var skinFile of skinFiles) {
 
+		var skinResults = [];
 		var dirty = false;
 
 		//Read the skin's JSON file.
 		var json = await fs.readJson(skinFile.absPath);
 
 		//console.log(json.font_name)
-		if(typeof channel === "undefined" || channel === "common" //If common or not passed
+		if((typeof channel === "undefined" || channel === "common") //If common or not passed
 		&& font !== json.font_name //And if the font is not already the one we want to set.
 		&& fontsWhitelist.indexOf(json.font_name) < 0 ){ //And if the font is not white-listed.
 
-			//Then set the default font at the root of the skin.
-			if(verbose)console.log((`Setting ${skinFile.theme}/${skinFile.name}`+
+			//console.log(json.font_name)
+			let message = `theme:${skinFile.theme}\tskin:${skinFile.name}`+
 				`\tplatform:common`+
 				`\ttype:${json.wType}`+
 				`\tfrom:${json.font_name}`+
-				`\tto:${font}`).debug);
+				`\tto:${font}`;
+
+			//Then set the default font at the root of the skin.
+			if(verbose)console.log(`Setting ${message}`.debug);
 			json.font_name = font;
 			dirty = true; //Write to file only if one or more platform fonts are set.
+
+			skinResults.push({
+				message: message,
+				color: "info"
+			});
 		}
 
 		//Get the fonts for each specific channel.
@@ -60,14 +71,21 @@ async function setFonts(font, projectPath, channel, theme, fontsWhitelist, force
 				&& font !== value.font_name //And if the font is not already the one we want to set.
 				&& fontsWhitelist.indexOf(value.font_name) < 0 ){ //And if the font is not white-listed.
 
-					//Then set the font for the specific platform.
-					if(verbose)console.log((`Setting ${skinFile.theme}/${skinFile.name}`+
+					let message = `theme:${skinFile.theme}\tskin:${skinFile.name}`+
 						`\tplatform:${key}`+
 						`\ttype:${json.wType}`+
 						`\tfrom:${json[key].font_name}`+
-						`\tto:${font}`).debug);
+						`\tto:${font}`;
+
+					//Then set the font for the specific platform.
+					if(verbose)console.log(message.debug);
 					json[key].font_name = font;
 					dirty = true; //Write to file only if one or more platform fonts are set.
+
+					skinResults.push({
+						message: message,
+						color: "info"
+					});
 				}
 			}
 		});
@@ -79,18 +97,32 @@ async function setFonts(font, projectPath, channel, theme, fontsWhitelist, force
 					spaces: 2
 				});
 				//if(verbose)console.log(`Updated ${skinFile.relPath}`.debug);
+				results = results.concat(skinResults.map((result) => {
+					result.message = `Changed ${result.message}`
+					result.color = "green";
+					return result;
+				}));
 			}
 			catch(e){
 				console.error(`Error updating skin file ${skinFile.relPath}: ${e}`.error);
+				results = results.concat(skinResults.map((result) => {
+					result.message = `Could not change ${result.message}`
+					result.color = "red";
+					return result;
+				}));
 			}
 		}
 		else if(dirty){ // and !force
 			//if(verbose)console.log(`Will not update without force option`.debug);
+			results = results.concat(skinResults.map((result) => {
+				result.message = `Would change ${result.message}`
+				result.color = "info";
+				return result;
+			}));
 		}
 	}
 
-
-	return [];//skinFonts;
+	return results;//skinFonts;
 }
 
 module.exports = setFonts;
