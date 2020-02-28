b42590aa79640ca7163d7de3f82b70722cf6c9b9
mig82 <miguelangelxfm@gmail.com>
2020-02-28 12:13:41 +0100

    Bump up major

--- src/cli/viskit.js
+++ src/cli/viskit.js
@@ -6,7 +6,7 @@ const theme = require("../core/config/theme");
 colors.setTheme(theme);
 
 program
-	.version('1.4')
+	.version('2.0-rc1')
 	.usage('[options] [command]')
 	.option('-v, --verbose', 'output everything');
 
